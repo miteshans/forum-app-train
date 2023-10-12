d14b6b5be23f62badc4e997f6307d7a8748636ae
miteshans <mitesh.chohan@ans.co.uk>
2023-10-12 16:34:05 +0100

    cleanup

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,31 +8,6 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <!-- <b>
-                Thread is the initial topic, posts are left in a thread (think of it as comments) admins can lock the thread and prevent other users from leaving posts in the locked thread.
-                </b><br><br>
-                <b>REQ (Must)</b> <br>
-                - <strike>User Reg</strike> <br>
-                - <strike>User profile - private to the user, should show the amount of threads and posts they’ve created</strike><br>
-                - (admin) Ability to lock thread <br>
-                - (admin) Ability to delete users <br>
-                <hr><br>
-                <b>REQ Stats/Interaction (Should)</b><br>
-                - Ability to track views of each thread <br>
-                - Display ranking on the landing page of the 10 most active users this month <br>
-                - The user should be able to LIKE the thread/post. Users who liked the post or a thread are displayed under the body of the thread or post, their email is shown along with the date when the LIKE was left.<br>
-                <hr><br>
-                <b>REQ Stats/Interaction (Should)</b><br>
-                - Add applicable unit tests to the functionality
-                <hr><br> -->
-
-                <!-- <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div> -->
-                <!-- <hr>
-                <div class="p-6 text-gray-900">
-                    <a href="/latest-threads">View Latest Threads</a>
-                </div> -->
                 <hr>
                 <div class="p-6 text-gray-900">
                     <a href="/user-threads">My Threads</a> | <a href="/add-a-thread">Create A Thread</a>
