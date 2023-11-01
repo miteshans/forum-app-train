ca6c480f894caf77c824cb4013d7b4ce7bc63494
miteshans <mitesh.chohan@ans.co.uk>
2023-11-01 10:49:42 +0000

    clean up

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,15 +1,12 @@
 <?php
 
 namespace Tests\Feature;
-use App\Models\User;
 
-//use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    //use RefreshDatabase;
-
     /**
      * A basic test example.
      */
@@ -18,44 +15,5 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
-    }
-
-    public function test_LoggedOut(): void
-    {
-        $response = $this->get('/latest-threads');
-
-        $response->assertStatus(302)
-            ->assertRedirect('/login');
-    }
-
-    public function test_mytesting(): void
-    {
-        
-        // $user = User::factory()->create();
-        
-        // $this->actingAs($user);
-        
-        // //$name = "John";
-        //  $name = "Jack";
-        //  echo $user->name;
-        // $this->assertTrue($name == "Jack");
-
-         // Create an authenticated user
-         $user = User::factory()->create();
-         $this->actingAs($user);
- 
-         $threadData = [
-             'thetitle' => 'Test Thread',
-             'thebody' => 'This is the thread body content.',
-         ];
- 
-         // route
-         $response = $this->post('/store-thread', $threadData);
- 
-
-         //$response->assertStatus(302); 
- 
-        // Assert
-        $response->assertRedirect('/add-a-thread');
     }
 }
