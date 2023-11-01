ee93d8bfdd3af3e74a11a073091292112a2945a1
miteshans <mitesh.chohan@ans.co.uk>
2023-11-01 11:06:28 +0000

    Playing with testing

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+use App\Models\User;
 
 class ExampleTest extends TestCase
 {
@@ -15,5 +16,36 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_mytesting(): void
+    {
+        
+        // $user = User::factory()->create();
+        
+        // $this->actingAs($user);
+        
+        // //$name = "John";
+        //  $name = "Jack";
+        //  echo $user->name;
+        // $this->assertTrue($name == "Jack");
+
+         // Create an authenticated user
+         $user = User::factory()->create();
+         $this->actingAs($user);
+ 
+         $threadData = [
+             'thetitle' => 'Test Thread',
+             'thebody' => 'This is the thread body content.',
+         ];
+ 
+        // route
+        $response = $this->post('/store-thread', $threadData);
+ 
+
+        //$response->assertStatus(302);
+ 
+        // Assert
+        $response->assertRedirect('/add-a-thread');
     }
 }
