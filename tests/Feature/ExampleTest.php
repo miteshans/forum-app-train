34c5cb92553fc43078c109f738d50f133d0067fe
miteshans <mitesh.chohan@ans.co.uk>
2023-11-02 10:19:59 +0000

    Tests for Threads

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,7 +4,7 @@ namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-use App\Models\User;
+
 
 class ExampleTest extends TestCase
 {
@@ -16,36 +16,5 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
-    }
-
-    public function test_mytesting(): void
-    {
-        
-        // $user = User::factory()->create();
-        
-        // $this->actingAs($user);
-        
-        // //$name = "John";
-        //  $name = "Jack";
-        //  echo $user->name;
-        // $this->assertTrue($name == "Jack");
-
-         // Create an authenticated user
-         $user = User::factory()->create();
-         $this->actingAs($user);
- 
-         $threadData = [
-             'thetitle' => 'Test Thread',
-             'thebody' => 'This is the thread body content.',
-         ];
- 
-        // route
-        $response = $this->post('/store-thread', $threadData);
- 
-
-        //$response->assertStatus(302);
- 
-        // Assert
-        $response->assertRedirect('/add-a-thread');
     }
 }
---
+++ tests/Feature/ThreadTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+
+class ThreadTest extends TestCase
+{
+
+    /** @test */
+    // Can create a thread if user is authenticated
+    public function can_create_threads_if_authenticated(): void
+    {
+        
+         // With an authenticated user
+        $user = User::factory()->create();
+        $this->actingAs($user);
+ 
+        $threadData = [
+            'thetitle' => 'Test Thread',
+            'thebody' => 'This is the thread body content.',
+         ];
+ 
+        // When
+        $response = $this->post('/store-thread', $threadData);
+ 
+        // Then
+        $response->assertRedirect('/add-a-thread');
+    }
+
+    /** @test */
+    // Cannot create a thread if user is NOT authenticated
+    public function cannot_create_threads_ifnot_authenticated(): void
+    {
+        // When
+        $response = $this->post('/store-thread');
+ 
+        // Then
+        $response->assertRedirect('/login');
+    }
+}
