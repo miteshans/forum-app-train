2805b0369a785b854a2becc6e93c7c7c490fc263
miteshans <mitesh.chohan@ans.co.uk>
2023-11-06 16:52:45 +0000

    non admin cannot lock thread Test

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -35,4 +35,28 @@ class UserTest extends TestCase
         $thread = Thread::latest()->first();
         $this->assertTrue($thread->isLocked($thread));
     }
+
+         /** @test */
+         public function non_admin_cannot_lock_thread()
+         {
+             // Create an admin user
+             $admin = User::factory()->create(['is_admin' => true]); 
+             $userid = $admin['id'];
+     
+             // Create a thread
+             Thread::create([
+                 'title' => 'Admin can lock a Thread',
+                 'body' => 'testAdminCanLockThread',
+                 'user_id' => $userid,
+                 'locked' => 0
+             ]);
+     
+             // get the last thread created and run the route to lock that thread
+             $thread = Thread::latest()->first();
+             $response = $this->post(route('lockthreadstore', [$thread]));
+     
+             // get the last thread created ie. it should now be locked
+             $thread = Thread::latest()->first();
+             $this->assertFalse($thread->isLocked($thread));
+         }
 }
